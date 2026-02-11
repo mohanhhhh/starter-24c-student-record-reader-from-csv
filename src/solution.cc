00079cd322816b1758fc61e3ffafc2067e8d325a
mohanhhhh <mohanh2@illinois.edu>
2026-02-11 16:46:07 -0600

    Update solution.cc

--- src/solution.cc
+++ src/solution.cc
@@ -5,34 +5,55 @@
 
 Student ReadStudentRecFromStream(std::istream& is) {
     Student student;
-    std::string firstName, lastName;
-    unsigned int uin;
-    char comma;
-    double gpa;
+    std::string firstName;
+    std::string lastName;
     
+
+    unsigned int uin = 0;
+    char comma = '\0';
+    double gpa = 0.0;
+    
+
     is >> firstName;
-    if (is.fail()) return Student{};
+    if (is.fail()) {
+        return Student{};
+    }
     
+
     is >> lastName;
-    if (is.fail()) return Student{};
+    if (is.fail()) {
+        return Student{};
+    }
     
+
     if (lastName.empty() || lastName.back() != ',') {
         return Student{};
     }
+    
+
     lastName.pop_back();
     
+
     student.full_name = firstName + " " + lastName;
     
+
     is >> uin;
-    if (is.fail()) return Student{};
+    if (is.fail()) {
+        return Student{};
+    }
     
+
     is >> comma;
-    if (is.fail() || comma != ',') return Student{};
+    if (is.fail() || comma != ',') {
+        return Student{};
+    }
     
+
     is >> gpa;
-    if (is.fail()) return Student{};
-    
-    student.uin = uin;
+    if (is.fail()) {
+        return Student{};
+    }
+        student.uin = uin;
     student.gpa = gpa;
     
     return student;
